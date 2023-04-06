c096ceb34a9c25e3d1f755b5b25680c24770f59a
Malcolmt129 <malcolm.t129@gmail.com>
2023-04-06 18:30:06 -0400

    I have finished within the specs of the assignment

--- zillow.js
+++ zillow.js
@@ -8,15 +8,23 @@ const db  = [{ price: 240000, city: "baltimore" }, { price: 300000, city: "austi
     { price: 250000, city: "boston" }]
 
 
-app.get('/v1/zillow/zestimate', (req, res) => {
+app.get('/v1/zillow/zestimate', async (req, res) => {
+    const { sqft, bed, bath } = req.query;
     
-    const sqft = req.query.sqft
-    const bed = req.query.bed
-    const bath = req.query.bath
-  
-    const zestimate = sqft * bed * bath * 10
-  
-    res.json({ zestimate })
+    //Check to see if we have enough of the parameters that we want
+    if (!sqft || !bed || !bath) {
+        res.status(404).json( {message: 'All parameters, sqft, bed, and bath are required'})
+    }
+
+    //Checking to see if the values of the query can be parsed into a number. If not, its a bad request
+    for (param in req.query) { 
+         if (isNaN(req.query[param])) {
+             res.status(404).json({message: `The query parameter ${param} is not a number`});
+         }
+    }
+
+    const zestimate = req.query.sqft * req.query.bed * req.query.bath * 10;
+    res.status(200).json({zestimate: zestimate});
   })
 
 app.get('/v1/zillow/houses', async (req, res) => {
@@ -31,12 +39,37 @@ app.get('/v1/zillow/houses', async (req, res) => {
         
     });
 
-
     res.status(200).send(houses)
+})
+
+app.get('/v1/zillow/prices', async (req, res) => {
+    const { usd } = req.query;
+
+    if (!usd) {
+        res.status(404).json( {message: 'The parameter usd is required'});
+    }
+
+    if (isNaN(req.query.usd)){
+        res.status(404).json({message: "The query parameter usd is not a number"});
+    }
+
+    houses = []
+
+    for (entry in db) { //price data is at db[entry].price
+        if (req.query.usd >= db[entry].price) {
+            houses.push(db[entry])
+        }
+    }
+
+    res.status(200).json(houses);
 })
 
 
 
+
+app.use('*', (req, res) => {
+    res.status(404).json({message: "This endpoint does not exist!"})
+})
 
 
 app.listen(port, () => {
